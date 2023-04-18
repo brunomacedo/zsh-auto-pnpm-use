47edf1b59ca105f3e23fcd3def32ad27f0cf17c8
brunomacedo <contato@brunomacedo.com.br>
2023-04-18 15:49:12 -0300

    feat(zsh): auto load pnpm node version

---
+++ README.md
@@ -0,0 +1,37 @@
+# zsh-auto-pnpm-use
+
+`zsh-auto-pnpm-use` is a zsh plugin that automatically loads the node version specified in `.npmrc` or `.nvmrc`.
+
+> `.npmrc` example:
+
+```shell
+node-version=18.16.0
+```
+
+or
+
+> `.nvmrc` example:
+
+```shell
+18.16.0
+```
+
+## Installation
+
+### As an [Oh My ZSH!](https://github.com/robbyrussell/oh-my-zsh) custom plugin
+Clone `zsh-auto-pnpm-use` into your custom plugins repo.
+
+```shell
+git clone https://github.com/brunomacedo/zsh-auto-pnpm-use ~/.oh-my-zsh/custom/plugins/zsh-auto-pnpm-use
+```
+
+Then load as a plugin in your `.zshrc`
+
+```shell
+plugins=(
+  # git
+  # zsh-autosuggestions
+  zsh-auto-pnpm-use
+  # zsh-auto-nvm-use
+)
+```
---
+++ zsh-auto-pnpm-use.plugin.zsh
@@ -0,0 +1,42 @@
+autoload -Uz add-zsh-hook
+
+load-pnpmrc() {
+  if (( ! $+commands[pnpm] )); then
+    return
+  fi
+
+  local global_node_version=$(node --version)
+  global_node_version=${global_node_version/v/}
+
+  local npmrc_file=".npmrc"
+  local nvmrc_file=".nvmrc"
+  local default_node_version=$(pnpm config get use-node-version)
+  local local_node_version=$(pnpm config get node-version)
+
+  if [[ -z "$local_node_version" && -f "$nvmrc_file" && -r "$nvmrc_file" ]]; then
+    local_node_version=$(cat "$nvmrc_file")
+  fi
+
+  if [[ ! -z "$local_node_version" ]]; then
+    echo "Node.js $local_node_version is activated"
+    pnpm env use --global "$local_node_version" > /dev/null
+    return
+  fi
+
+  if [[ "$global_node_version" != "$default_node_version" ]]; then
+    echo "Reverting to default node version $default_node_version"
+    pnpm env use --global "$default_node_version" > /dev/null
+    return
+  fi
+}
+
+auto-load-pnpmrc() {
+  local current_dir=$(pwd -P)
+
+  if [[ "$current_dir" != "$PREV_PWD" ]]; then
+    PREV_PWD="$current_dir"
+    load-pnpmrc
+  fi
+}
+
+add-zsh-hook precmd auto-load-pnpmrc
